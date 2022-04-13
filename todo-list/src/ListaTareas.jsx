7f3067b80820ffceb5b516fd27d102e3f9b3c242
Angatupyry <fierrofenix@gmail.com>
2022-04-13 08:45:02 -0400

    set state

--- todo-list/src/ListaTareas.jsx
+++ todo-list/src/ListaTareas.jsx
@@ -1,17 +1,41 @@
 import Tarea from "./Tarea";
-const tareas = [
-  { titulo: "Pasear a mi perrito" },
-  { titulo: "Tomar agua" },
-  { titulo: "Dormir" },
+import { useState } from "react";
+const tareasIniciales = [
+  { id: 1, titulo: "Ir al cine" },
+  { id: 2, titulo: "Pasear el perro" },
+  { id: 3, titulo: "Comprar fruta" },
+  { id: 4, titulo: "Tomar agua" },
 ];
 
 const ListaTareas = () => {
+  const [tareas, setTareas] = useState(tareasIniciales);
+
+  const addTask = () => {
+    const newTask = {
+      id: Math.floor(Math.random() * 100),
+      titulo: "Nueva Tarea",
+    };
+
+    const newTasks = [...tareas, newTask];
+
+    // for (let index = 0; index < tareas.length; index++) {
+    //   newTasks.push(tareas[index]);
+    // }
+
+    // newTasks.push(newTask);
+    setTareas(newTasks);
+  };
+
   return (
-    <ul id="lista-tareas">
-      {tareas.map((tarea) => {
-        return <Tarea titulo={tarea.titulo} />;
-      })}
-    </ul>
+    <>
+      <ul id="lista-tareas">
+        {tareas.map((tarea) => {
+          return <Tarea titulo={tarea.titulo} />;
+        })}
+      </ul>
+      <button onClick={addTask}>Add task</button>
+      <button>Remove task</button>
+    </>
   );
 };
 
