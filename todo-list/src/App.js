e267abf050b5d7ef8aca00a9d349ce31d0bf460c
Angatupyry <fierrofenix@gmail.com>
2022-04-13 08:05:55 -0400

    State and setState init

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import ListaTareas from "./ListaTareas";
 import FormListaTarea from "./FormListaTarea";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
     <div className="App">
       <main>
         <h1>Lista de tareas</h1>
+        <Counter titulo="Hola. Soy un counter :D" subtitulo="Subtitulo" />
         <FormListaTarea />
         <h3>Tareas</h3>
         <ListaTareas />
